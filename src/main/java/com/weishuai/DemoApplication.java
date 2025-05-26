c1b654a56ad28fae0870cee4f96ce0421171471d
YD03901148-魏帅 <weishuai@yundingkeji.cn>
2025-05-27 06:54:53 +0800

    refactor/util: 更新代码行统计工具的项目路径
    - 修改了 CodeLineCounter 和 JavaProjectLineCounter 两个类中的项目路径
    - 新增了对 pegasus-java-boot 项目的统计路径
    - 注释掉了原有的 northwest-park-service 和 fire_fighting_and_security_dispatch_api 项目的路径
    - 删除了未使用的 HELP.md 文件

--- src/main/java/com/weishuai/DemoApplication.java
+++ src/main/java/com/weishuai/DemoApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "com.weishuai.mapper")
+@MapperScan(basePackages = "com.weishuai")
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/weishuai/util/oom/OomException.java
+++ src/main/java/com/weishuai/util/oom/OomException.java
@@ -1,8 +1,6 @@
 package com.weishuai.util.oom;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class OomException {
